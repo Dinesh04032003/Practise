2617ed9ed36407b2f991136aa51f6950e5cf0a7f
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-09-20 12:13:59 +0530

    Add files via upload

---
+++ sample1.java
@@ -0,0 +1,21 @@
+class Car {
+
+	String brand;
+	
+	//your constructor here
+	
+	public String getBrand() {
+		return brand;
+	}
+
+	void run() {
+		System.out.println("Car is running...");
+	}
+}
+
+public class Sample1 {
+	
+	public static void main(String[] args) {
+		Car ford = new Car();			
+	}
+}
