150ec195b21d01331019b81f7f3e9f0aeef88f98
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-02 12:44:05 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class even 
+{
+    even()
+    {
+        Scanner din=new Scanner(System.in);
+        System.out.println("enter the number");
+        int a=din.nextInt();
+        int i=1;
+        while(i<=a)
+        {
+            if()
+            {
+                System.out.println(i);
+            }
+            i=i+1;
+        }
+        
+    }
+}
+class main 
+{
+    public static void main(String args[])
+    {
+        even c1=new even();
+    }
+}
+    
---
+++ table.java
@@ -0,0 +1,22 @@
+class Sum 
+{
+    Sum()
+    {
+        int a=10;
+        int i=1;
+        System.out.println(a+" table");
+        while(i<=a)
+        {
+            System.out.println(a*i);
+            i+=1;
+        }
+    }
+}
+class table
+{
+    public static void main (String[] args) 
+    {
+        Sum obj=new Sum();
+    }
+    
+}
