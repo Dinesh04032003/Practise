959c6d0bdd191ef23ad754410dd0c483830df159
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-07 12:07:01 +0530

    Add files via upload

---
+++ arfor.java
@@ -0,0 +1,23 @@
+class arr 
+{
+    arr()
+    {
+        int n[]=new int[4];
+        n[0]=1;
+        n[1]=8;
+        n[2]=7;
+        n[3]=5;
+        for (int i=0;i<4;i++)
+        {
+            System.out.println(n[i]);
+        }
+        
+    }
+}
+class arfor
+{
+public static void main (String[] args) 
+    {
+        arr obj=new arr();
+    }
+}
