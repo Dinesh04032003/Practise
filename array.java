44f8bf2045a8378987721c4ac520cdc23d12c098
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-07 12:06:38 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,22 @@
+class arr 
+{
+    arr()
+    {
+        int n[]=new int[4];
+        n[0]=1;
+        n[1]=8;
+        n[2]=7;
+        n[3]=5;
+        System.out.prinln(n[0]);    
+        System.out.prinln(n[1]);
+        System.out.prinln(n[2]);    
+        System.out.prinln(n[3]);        
+    }
+}
+class array
+{
+public static void main (String[] args) 
+    {
+        arr obj=new arr();
+    }
+}
