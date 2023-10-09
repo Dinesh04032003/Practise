13b3a127d2f53965eaad282a66aaa4dbd7ae5c2c
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-09 20:09:13 +0530

    Add files via upload

---
+++ delete.java
@@ -0,0 +1,16 @@
+class java 
+{
+    java()
+    {
+       StringBuilder sb=new StringBuilder("Hello");  
+       sb.delete(1,3);  
+       System.out.println(sb);
+    }
+}
+class delete
+{
+public static void main (String[] args) 
+{
+    java c1=new java();
+}
+}
