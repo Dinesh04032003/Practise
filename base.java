cf07dfbd8edc11cb15f35ec3c17c91df51ac33dc
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-10 01:13:00 +0530

    Add files via upload

---
+++ base.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class java 
+{
+    java()
+    {
+        System.out.println("PRAVEEN");
+        System.out.println("sURAJ");
+    }
+    
+}
+class base
+{
+public static void main (String[] args) 
+{
+    java c1=new java();
+}
+}
