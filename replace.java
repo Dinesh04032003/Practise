d6379229b54ea2f1773bf77c4ee7868071c9b117
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-09 20:04:39 +0530

    Add files via upload

---
+++ replace.java
@@ -0,0 +1,16 @@
+class java 
+{
+    java()
+    {
+       StringBuilder sb=new StringBuilder("Hello");  
+       sb.replace(1,3,"Java");  
+       System.out.println(sb);
+    }
+}
+class replace
+{
+public static void main (String[] args) 
+{
+    java c1=new java();
+}
+}
