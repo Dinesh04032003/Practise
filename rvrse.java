fae80b577323937bf113064d2b0bd0dce36148ae
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-09 20:12:33 +0530

    Add files via upload

---
+++ rvrse.java
@@ -0,0 +1,16 @@
+class java 
+{
+    java()
+    {
+       StringBuilder sb=new StringBuilder("Hello");  
+       sb.reverse();  
+       System.out.println(sb);
+    }
+}
+class rvrse
+{
+public static void main (String[] args) 
+{
+    java c1=new java();
+}
+}
