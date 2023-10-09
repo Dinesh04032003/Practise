39e58d857536f76a2ed19e08e61dcd53e5ec7be2
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-09 20:36:01 +0530

    Add files via upload

---
+++ parmif.java
@@ -0,0 +1,21 @@
+class java 
+{
+    java(int x,int y)
+    {
+        if(x==y)
+        {
+            System.out.println("They are equal");
+        }
+        else
+        {
+            System.out.println("They are not equal");
+        }
+    }
+}
+class parmif
+{
+public static void main (String[] args) 
+{
+    java c1=new java(10,20);
+}
+}
