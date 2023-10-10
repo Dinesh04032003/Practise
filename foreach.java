7e6e31c402eee679873801b17372a3275e65cd9b
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-10 11:56:08 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class java
+{
+    java()
+    {
+     int n[]={1,2,3,4,5};
+     for(int i:n)
+     {
+         System.out.println(i);
+     }
+    }
+}
+class foreach
+{
+    public static void main(String arg[])
+    {
+        java sc=new java();
+    }
+}
