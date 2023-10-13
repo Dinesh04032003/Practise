858bbbe7b2c332428a895cdf3011ca19ea63336d
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-13 14:36:57 +0530

    Add files via upload

---
+++ mul.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+class java1
+{
+    java1()
+    {
+      int a=10;
+      int b=20;
+      System.out.println(Math.multiplyExact(a,b));
+}
+}
+class mul
+{
+    public static void main(String args[])
+    {
+        java1 obj=new java1();
+    }
+}
