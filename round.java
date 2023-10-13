2a4800d2d4b5f4c711bf39cfe64fbee2bda33ed0
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-13 14:44:04 +0530

    Add files via upload

---
+++ round.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+class java1
+{
+    java1()
+    {
+      float a=10.6f;
+      System.out.println(Math.round(a));
+}
+}
+class round
+{
+    public static void main(String args[])
+    {
+        java1 obj=new java1();
+    }
+}
