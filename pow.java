7ee5902e6b102d3dff69acbe59bf1f19c8bb6689
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-13 14:57:28 +0530

    Add files via upload

---
+++ pow.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+class java1
+{
+    java1()
+    {
+      int a=2;
+      int b=5;
+      System.out.println(Math.pow(a,b));
+}
+}
+class pow
+{
+    public static void main(String args[])
+    {
+        java1 obj=new java1();
+    }
+}
