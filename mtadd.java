3da13339cc0afff173b54cec3b5eaccfe9a35e2e
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-13 14:49:00 +0530

    Add files via upload

---
+++ mtadd.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+class java1
+{
+    java1()
+    {
+      int a=10;
+      int b=50;
+      System.out.println(Math.addExact(a,b));
+}
+}
+class mtadd
+{
+    public static void main(String args[])
+    {
+        java1 obj=new java1();
+    }
+}
