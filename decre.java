4bbc1b7634d33d7729628721b09abc54a1586964
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-13 14:38:59 +0530

    Add files via upload

---
+++ decre.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+class java1
+{
+    java1()
+    {
+      int a=10;
+      System.out.println(Math.decrementExact(a));
+}
+}
+class decre 
+{
+    public static void main(String args[])
+    {
+        java1 obj=new java1();
+    }
+}
