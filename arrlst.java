ff170a513d898ec1d45dc94b597b9fdc84b2fb00
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-14 18:14:37 +0530

    Add files via upload

---
+++ arrlst.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class java1
+{
+    java1()
+    {
+       ArrayList din=new ArrayList();
+       din.add(10);
+       din.add(30);
+       din.add(40);
+       System.out.println(din);
+    }
+
+}
+class arrlst
+{
+    public static void main(String args[])
+    {
+        java1 obj=new java1();
+    }
+}
