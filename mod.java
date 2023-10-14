3e80d4782be471f245a31628ffa5351b8a10b9e2
Dinesh04032003 <145021627+Dinesh04032003@users.noreply.github.com>
2023-10-14 10:56:18 +0530

    Add files via upload

---
+++ mod.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+class java1
+{
+    java1()
+    {
+      int a=20;
+      int b=3;
+      System.out.println(Math.floorMod(a,b));
+}
+}
+class mod
+{
+    public static void main(String args[])
+    {
+        java1 obj=new java1();
+    }
+}
